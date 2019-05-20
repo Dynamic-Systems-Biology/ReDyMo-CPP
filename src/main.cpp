47e706ac01456e9c193970e0c7427b75b4dc06a5
Bruno B. Scholl <bruno.scholl@usp.br>
2019-05-20 16:03:18 -0300

    Add Parameter summary
    Signed-off-by: Bruno B. Scholl <bruno.scholl@usp.br>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "s_phase.hpp"
 #include <algorithm>
 #include <ctime>
+#include <iomanip>
 #include <iostream>
-#include <sstream>
 #include <omp.h>
+#include <sstream>
 
 int get_cmd_option(char **input, int size, const std::string &option)
 {
@@ -26,26 +27,48 @@ int main(int argc, char *argv[])
     {
         if (!cmd_option_exists(argv, argv + argc, option))
         {
-            std::cout << "Incorrect arguments. For usage see README.md" << std::endl;
+            std::cout << "Incorrect arguments. For usage see README.md"
+                      << std::endl;
             exit(0);
         }
     }
 
-    int index            = get_cmd_option(argv, argc, arg_options[0]);
-    int n_cells          = atoi(argv[index]);
+    std::cout << std::endl
+              << "Parameter summary ==========================================="
+              << std::endl;
+    int index   = get_cmd_option(argv, argc, arg_options[0]);
+    int n_cells = atoi(argv[index]);
+    std::cout << std::left << std::setw(21) << "Number of cells: " << std::right
+              << std::setw(40) << n_cells << std::endl;
+
     index                = get_cmd_option(argv, argc, arg_options[1]);
     std::string organism = argv[index];
-    index                = get_cmd_option(argv, argc, arg_options[2]);
-    int n_resources      = atoi(argv[index]);
-    index                = get_cmd_option(argv, argc, arg_options[3]);
-    int speed            = atoi(argv[index]);
-    index                = get_cmd_option(argv, argc, arg_options[4]);
-    int timeout          = atoi(argv[index]);
-    index                = get_cmd_option(argv, argc, arg_options[5]);
+    std::cout << std::left << std::setw(21) << "Organism: " << std::right
+              << std::setw(40) << organism << std::endl;
 
+    index           = get_cmd_option(argv, argc, arg_options[2]);
+    int n_resources = atoi(argv[index]);
+    std::cout << std::left << std::setw(21) << "Number of forks: " << std::right
+              << std::setw(40) << n_resources << std::endl;
+
+    index     = get_cmd_option(argv, argc, arg_options[3]);
+    int speed = atoi(argv[index]);
+    std::cout << std::left << std::setw(21)
+              << "Steps per iteration: " << std::right << std::setw(40) << speed
+              << std::endl;
+
+    index       = get_cmd_option(argv, argc, arg_options[4]);
+    int timeout = atoi(argv[index]);
+    std::cout << std::left << std::setw(21) << "Max iterations: " << std::right
+              << std::setw(40) << timeout << std::endl;
+
+    index = get_cmd_option(argv, argc, arg_options[5]);
     bool dormant;
     std::stringstream ss(argv[index]);
     ss >> std::boolalpha >> dormant;
+    std::cout << std::left << std::setw(21)
+              << "Use dormant origins: " << std::right << std::setw(40)
+              << dormant << std::endl << std::endl;
 
     std::shared_ptr<DataManager> data = std::make_shared<DataManager>(
         "../data/simulation.sqlite", "../data/MFA-Seq_TBrucei_TREU927/");
