b1a96000ff1bdfd923506c230d767423cd0a98c6
Bruno B. Scholl <bruno.scholl@cobli.co>
2021-03-22 12:42:44 -0300

    Create script to decompress and plot result files

---
+++ script/visualize.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+import sys
+import subprocess
+import os
+from matplotlib import pyplot as plt
+
+compressed_file_name=os.path.abspath(sys.argv[1])
+uncompressed_file_name=compressed_file_name[:-4]
+
+subprocess.call('zstd -d -f ' + compressed_file_name + ' -o ' + uncompressed_file_name, shell=True)
+
+with open(uncompressed_file_name, "r") as file:
+  # step between points to be plotted. increase it to reduce time (will mess with y axis values)
+  step = 1
+  times=file.readlines()
+  count = 0
+  reduced = []
+
+  # plot one every step
+  for t in times:
+    if count >= step:
+      count = 0
+      reduced.append(t)
+    count = count + 1
+
+  # configure plot ticks to not bunch up
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1)
+  max_yticks = 30
+  yloc = plt.MaxNLocator(max_yticks)
+  ax.yaxis.set_major_locator(yloc)
+
+  plt.plot(reduced, '.g', markersize=0.3)
+  plt.show()
