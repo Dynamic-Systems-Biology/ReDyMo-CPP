cab9db1234335873eafc5bbbd55ddb7ee3f22cb2
Ligia <ligiapalma@usp.br>
2021-04-11 16:44:44 -0300

    Add test for generation

--- test/test_evolution.cpp
+++ test/test_evolution.cpp
@@ -6,7 +6,6 @@
 class MockEvolutionManager : public EvolutionManager
 {
   public:
-
     MOCK_METHOD(void, simulate, (), (override));
     MOCK_METHOD(void, reproduce, (), (override));
 
@@ -51,7 +50,7 @@ TEST_F(EvolutionTest, CalculateFitness)
 TEST_F(EvolutionTest, TestConstructor)
 {
     std::vector<char *> argv_mock = {
-        "program_name", "--cells", "2",         "--organism",
+        "program_name", "--cells", "2",         "--organism", "dummy",
         "--resources",  "2",       "--timeout", "5"};
 
     // Reset getopt global variable
@@ -61,8 +60,19 @@ TEST_F(EvolutionTest, TestConstructor)
     ASSERT_EQ(evo.get_arguments(), config.arguments());
 }
 
-TEST_F(EvolutionTest, TestGeneration) {
+TEST_F(EvolutionTest, TestGeneration)
+{
+    std::vector<char *> argv_mock = {
+        "program_name", "--cells", "2",         "--organism", "dummy",
+        "--resources",  "2",       "--timeout", "5"};
 
+    // Reset getopt global variable
+    optind               = 1;
+    Configuration config = Configuration(argv_mock.size(), argv_mock.data());
+    MockEvolutionManager evo(config, 0);
+    EXPECT_CALL(evo, simulate());
+    EXPECT_CALL(evo, reproduce());
+    evo.generation();
 }
 
 int main(int argc, char **argv)
